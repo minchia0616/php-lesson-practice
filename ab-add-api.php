61e356d5d38e457c7e6d187f86abff0835b0e6be
chia <chia87616@gmail.com>
2022-05-25 22:17:59 +0800

    ab-add-api

---
+++ ab-add-api.php
@@ -0,0 +1,39 @@
+<?php
+require __DIR__ . '/parts/connect-db.php';
+
+$output = [
+    'success' => false,     //預設沒有新增成功
+    'postData' => $_POST,   //把前端丟到後端的資料再丟回去
+    'error' => ''
+];
+
+// TODO: 欄位檢查, 後端的檢查
+
+$sql = "INSERT INTO `address_book`(
+    `name`, `mobile`, `email`, 
+    `birthday`, `address`, `created_at`
+    ) VALUES (
+        ?, ?, ?,
+        ?, ?, NOW()
+    )";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([            //真正的執行新增資料
+    $_POST['name'],
+    $_POST['mobile'],
+    $_POST['email'],
+    $_POST['birthday'],
+    $_POST['address'],
+]);
+
+
+if ($stmt->rowCount() == 1) {
+    $output['success'] = true;          //成功新增
+} else {
+    $output['error'] = '資料無法新增';
+}
+
+
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
