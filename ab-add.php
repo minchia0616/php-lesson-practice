e94029fccd37bf62396e4a6d1ee99731648a0365
chia <chia87616@gmail.com>
2022-05-26 23:13:53 +0800

    資料成功送出的訊息提示

--- ab-add.php
+++ ab-add.php
@@ -27,17 +27,17 @@ $title = '新增通訊錄資料';
                         <div class="mb-3">
                             <label for="name" class="form-label">* name</label>
                             <input type="text" class="form-control" id="name" name="name" required> <!-- required必填 -->
-                            <div class="form-text">提示字元</div>
+                            <div class="form-text red"><!--提示字元--></div>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">email</label>
                             <input type="email" class="form-control" id="email" name="email">
-                            <div class="form-text"></div>
+                            <div class="form-text red "></div>
                         </div>
                         <div class="mb-3">
                             <label for="mobile" class="form-label">mobile</label>
                             <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{8}">    <!-- 格式09開頭+8位數 -->
-                            <div class="form-text"></div>
+                            <div class="form-text red"></div>
                         </div>
                         <div class="mb-3">
                             <label for="birthday" class="form-label">birthday</label>
@@ -52,6 +52,10 @@ $title = '新增通訊錄資料';
 
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
+                    <!-- 新增成功的訊息提示，一開始先display:none -->
+                    <div id="info-bar" class="alert alert-success" role="alert" style="display:none;">
+                        資料新增成功
+                    </div>
 
                 </div>
             </div>
@@ -65,6 +69,7 @@ $title = '新增通訊錄資料';
     const email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/;
     const mobile_re = /^09\d{2}-?\d{3}-?\d{3}$/;
 
+    const info_bar = document.querySelector('#info-bar');
     const name_f = document.form1.name;
     const email_f = document.form1.email;
     const mobile_f = document.form1.mobile;
@@ -81,9 +86,10 @@ $title = '新增通訊錄資料';
 
         for(let i in fields){                   // 讓欄位的外觀回復原來的狀態
             fields[i].classList.remove('red');
-            fieldTexts[i].classList.remove('red');
             fieldTexts[i].innerText='';
         }
+
+        info_bar.style.display = 'none'; // 隱藏訊息列
 
         
         let isPass = true;                      // 預設為通過檢查
@@ -93,19 +99,16 @@ $title = '新增通訊錄資料';
             // name_f.nextElementSibling.classList.add('red');                              //拿到input的下一個(提示字元)
             // name_f.closest('.mb-3').querySelector('.form-text').classList.add('red');    closest往上找mb-3再往內找到form-text
             fields[0].classList.add('red');
-            fieldTexts[0].classList.add('red');
             fieldTexts[0].innerText='姓名至少兩個字';
             isPass = false;
         }
         if (email_f.value && !email_re.test(email_f.value)) {           // 信箱有填可是格式錯誤
             fields[1].classList.add('red');
-            fieldTexts[1].classList.add('red');
             fieldTexts[1].innerText='email 格式錯誤';
             isPass = false;
         }
         if (mobile_f.value && !mobile_re.test(mobile_f.value)) {        // 手機有填可是格式錯誤
             fields[2].classList.add('red');
-            fieldTexts[2].classList.add('red');
             fieldTexts[2].innerText='手機號碼格式錯誤';
             isPass = false;
         }
@@ -122,6 +125,21 @@ $title = '新增通訊錄資料';
         const result = await r.json();
         console.log(result);
 
+        info_bar.style.display = 'block'; // 顯示訊息列
+
+        if (result.success) {                           //success從後端抓，$output的結果會是json格式(拿到true和false)
+            info_bar.classList.remove('alert-danger');
+            info_bar.classList.add('alert-success');
+            info_bar.innerText = '新增成功';
+
+            setTimeout(() => {
+                location.href = 'ab-list.php'; // 跳轉到列表頁
+            }, 3000);
+        } else {
+            info_bar.classList.remove('alert-success');
+            info_bar.classList.add('alert-danger');
+            info_bar.innerText = result.error || '資料無法新增';
+        }
 
     }
 </script>
