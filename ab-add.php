6bf98f164a9884cb254d78cd922e3f2fae7c42f0
chia <chia87616@gmail.com>
2022-05-26 17:29:15 +0800

    欄位檢查的註解

--- ab-add.php
+++ ab-add.php
@@ -1,11 +1,10 @@
 <?php require __DIR__ . '/parts/connect-db.php';
 $pageName = 'ab-add';
 $title = '新增通訊錄資料';
-?>
 
+?>
 <?php include __DIR__ . '/parts/html-head.php' ?>
 <?php include __DIR__ . '/parts/navbar.php' ?>
-
 <div class="container">
     <div class="row">
         <div class="col-md-6">
@@ -26,7 +25,7 @@ $title = '新增通訊錄資料';
                         </div>
                         <div class="mb-3">
                             <label for="mobile" class="form-label">mobile</label>
-                            <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{8}"> <!-- 格式09開頭+8位數 -->
+                            <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{8}">    <!-- 格式09開頭+8位數 -->
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
@@ -52,7 +51,6 @@ $title = '新增通訊錄資料';
 <?php include __DIR__ . '/parts/scripts.php' ?>
 
 <script>
-    //先定義
     const email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/;
     const mobile_re = /^09\d{2}-?\d{3}-?\d{3}$/;
 
@@ -60,30 +58,38 @@ $title = '新增通訊錄資料';
     const email_f = document.form1.email;
     const mobile_f = document.form1.mobile;
 
+
     //前端的欄位格式檢查
     async function sendData() {
-        let iSPass = true ;                         //預設為通過檢查
-        if(name_f.value.length < 2){                //名字少於兩個字就不通過
+        // TODO: 欄位檢查, 前端的檢查
+        let isPass = true;                      // 預設為通過檢查
+
+        if (name_f.value.length < 2) {          // 名字少於兩個字就不通過
             alert('姓名至少兩個字');
             isPass = false;
         }
-        if(email_f.value && !email_re.test(email_f.value)){                //信箱有填可是格式錯誤
-            alert('信箱格式錯誤');
+        if (email_f.value && !email_re.test(email_f.value)) {           // 信箱有填可是格式錯誤
+            alert('email 格式錯誤');
             isPass = false;
         }
-        if(mobile_f.value && !mobile_re.test(mobile_f.value)){             //手機有填可是格式錯誤
+        if (mobile_f.value && !mobile_re.test(mobile_f.value)) {        // 手機有填可是格式錯誤
             alert('手機號碼格式錯誤');
             isPass = false;
         }
-        if (!isPass) {      //只要其中一項沒有通過檢查就直接結束函式，表單不會送出
+
+        if (!isPass) {                                                  // 只要其中一項沒有通過檢查就直接結束函式，表單不會送出
             return;
         }
+
         const fd = new FormData(document.form1);
         const r = await fetch('ab-add-api.php', {
             method: 'POST',
             body: fd,
-        })
+        });
         const result = await r.json();
         console.log(result);
+
+
     }
 </script>
+<?php include __DIR__ . '/parts/html-foot.php' ?>
