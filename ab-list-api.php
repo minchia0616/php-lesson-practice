00950e753feaafce11019d69c5d5e6dab81697a6
chia <chia87616@gmail.com>
2022-05-27 23:02:42 +0800

    ab-list-api 拿到資料用js呈現

---
+++ ab-list-api.php
@@ -0,0 +1,40 @@
+<?php require __DIR__ . '/parts/connect-db.php';
+$pageName = 'ab-list';
+$title = '通訊錄列表';
+
+$perPage = 20; // 每一頁有幾筆
+
+// 用戶要看第幾頁
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+if ($page < 1) {
+    header('Location: ?page=1');
+    exit;
+}
+
+$t_sql = "SELECT COUNT(1) FROM address_book";
+$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0]; // 總筆數
+
+$totalPages = ceil($totalRows / $perPage); // 總共有幾頁
+
+$rows = [];
+
+if ($totalRows > 0) {
+    // 頁碼若超過總頁數
+    if ($page > $totalPages) {
+        header("Location: ?page=$totalPages");
+        exit;
+    }
+
+    $sql = sprintf("SELECT * FROM address_book ORDER BY sid DESC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
+    $rows = $pdo->query($sql)->fetchAll();
+}
+
+$output = [
+    'perPage' => $perPage,
+    'page' => $page,
+    'totalRows' => $totalRows,
+    'totalPages' => $totalPages,
+    'rows' => $rows,
+];
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
